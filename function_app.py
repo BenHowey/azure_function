19fb2f3a8243139809f99d1857abd1d860bde838
bhowey1988 <ben_howey@rnli.org.uk>
2024-10-01 09:21:02 +0100

    updated auth

--- function_app.py
+++ function_app.py
@@ -94,7 +94,7 @@ def clean_data(data):
 
 
 @app.function_name(name="HttpTrigger1")
-@app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="hello", auth_level=func.AuthLevel.FUNCTION)
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     return func.HttpResponse(
